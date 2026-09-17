0a3cdb687e8896667acd65548fe3671b395b8728
Rebecca <rebecca.horvathsunden@gmail.com>
2026-09-17 09:24:23 +0200

    Användaren kan inte ange mindre än 1 gäst Användaren kan inte ange ett datum som passerat Användaren måste ha fyllt i formuläret korrekt för att bokningen ska gå igenom Lagt till felmeddelanden så användaren vet vad som behöver ändras Lagt till meddelanden när användaren trycker submit

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -5,10 +5,28 @@ function BookingForm() {
   const today = new Date().toISOString().slice(0, 10);
   const [date, setDate] = useState(today);
   const [time, setTime] = useState("14:00");
+  const [showPartySizeError, setShowPartySizeError] = useState(false);
+  const [showDateError, setShowDateError] = useState(false);
+  const [showTimeError, setShowTimeError] = useState(false);
+  const [showSubmitApproved, setShowSubmitApproved] = useState(false);
+  const [showSubmitDenied, setShowSubmitDenied] = useState(false);
 
   function handleOnSubmit(e) {
     e.preventDefault();
-    console.log("Hej");
+
+    if (Number(partySize) >=1 && showDateError !== true && time !== "") {
+      setShowSubmitDenied(false);
+      setShowSubmitApproved(true);
+      
+    } else {
+      setShowSubmitApproved(false);
+      setShowSubmitDenied(true);
+    }
+  }
+
+  function takeAwaySubmitErrors (){
+        setShowSubmitApproved(false)
+        setShowSubmitDenied(false)
   }
 
   return (
@@ -17,19 +35,60 @@ function BookingForm() {
         <input
           type="date"
           value={date}
-          onChange={(e) => setDate(e.target.value)}
+          onChange={(e) => {
+            takeAwaySubmitErrors();
+
+            if (e.target.value >= today) {
+              setDate(e.target.value);
+              setShowDateError(false);           
+            } else {
+              setShowDateError(true);
+            }
+          }}
         ></input>
+
         <input
           type="time"
           value={time}
-          onChange={(e) => setTime(e.target.value)}
+          onChange={(e) => {
+            takeAwaySubmitErrors();
+
+            if (e.target.value !== "") {
+              setTime(e.target.value);
+              setShowTimeError(false);
+            } else {
+              setTime(e.target.value);
+              setShowTimeError(true);
+            }
+          }}
         ></input>
+
         <input
           type="number"
+          min="1"
           value={partySize}
-          onChange={(e) => setPartySize(e.target.value)}
+          onChange={(e) => {
+            takeAwaySubmitErrors();
+
+            if (Number(e.target.value) >= 1) {
+              setPartySize(e.target.value);
+              setShowPartySizeError(false);
+            } else {
+              setPartySize(e.target.value);
+              setShowPartySizeError(true);
+            }
+          }}
         ></input>
+
+        {showDateError && <p>Du kan inte välja ett datum som passerat</p>}
+        {showTimeError && <p>Du måste välja en tid</p>}
+        {showPartySizeError && <p>Du måste välja minst 1 gäst</p>}
+
         <button type="submit">Skicka bokningsförfrågan</button>
+        
+        {showSubmitApproved && <p>Din bokningsförfrågan är skickad</p>}
+        {showSubmitDenied && <p>Se över din bokningsförfrågan och prova igen</p>}
+      
       </form>
     </>
   );
