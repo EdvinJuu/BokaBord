58b31409ed98dc78b09c0277e180afd951fae8ea
Edvin Juujärvi <edvin.juujarvi@hotmail.com>
2026-09-24 10:20:12 +0200

    added RestaurantDetails with id routing, moved UXDiskussion to /resources

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import RestaurantList from "./components/RestaurantList";
 import BookingForm from "./components/BookingForm";
 import { Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import RestaurantDetails from "./pages/RestaurantDetails";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/restaurant/:id" element={<RestaurantDetails/>} />
       </Routes>
     </>
   );
