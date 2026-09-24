6ea3f8a47a68e1f5cc6a9f7dbd8c1fee250e49da
Alice <alice.tanum@gmail.com>
2026-09-24 10:30:10 +0200

    added dashboard to app.tsx and dashboard file in pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Navbar } from "./components/Navbar";
-import RestaurantList from "./components/RestaurantList";
-import BookingForm from "./components/BookingForm";
 import { Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Dashboard" element={<Dashboard/>}/>
       </Routes>
     </>
   );
